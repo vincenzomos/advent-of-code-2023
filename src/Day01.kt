dcd4a56bb1751bae16860c2d5a8c2f7c36cdb191
Vincent Mos <vincenzomos@gmail.com>
2023-12-03 19:30:49 +0100

    AoC23: solution Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,76 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+enum class DigitEnum(val value: String, val digit: Char) {
+    ONE("one", '1'),
+    TWO("two", '2'),
+    THREE("three", '3'),
+    FOUR("four", '4'),
+    FIVE("five", '5'),
+    SIX("six", '6'),
+    SEVEN("seven", '7'),
+    EIGHT("eight", '8'),
+    NINE("nine", '9');
+
+    fun intValue(): Int {
+        return digit.digitToInt()
     }
+}
+class IndexContainingDigit(val index: Int, val digitEnum: DigitEnum)
+
+fun extractAllIndexesContainingStringDigits(rawValue: String, digitEnum: DigitEnum): List<IndexContainingDigit> {
+    val indices = Regex(digitEnum.value.toString()).findAll(rawValue)
+        .map { indexes ->  IndexContainingDigit(indexes.range.first, digitEnum) }
+
+        .toList()
+
+    return indices
+}
+fun extractAllIndexesContainingDigits(rawValue: String, digitEnum: DigitEnum): List<IndexContainingDigit> {
+    val indices = Regex(digitEnum.digit.toString()).findAll(rawValue)
+        .map { indexes ->  IndexContainingDigit(indexes.range.first, digitEnum) }
+        .toList()
+    return indices
+}
+
+
+fun getCalibrationValue(rawValue: String) :Int {
+    val mapForNumericDigits = DigitEnum.values().map { it -> extractAllIndexesContainingDigits(rawValue, it) }
+        .flatMap { it }.groupBy { it -> it.index }
+
+    val mapForStringDigits = DigitEnum.values().map { it -> extractAllIndexesContainingStringDigits(rawValue, it) }
+        .flatMap { it }.groupBy { it -> it.index }
+
+    //union maps
+    val totalMap = mapForNumericDigits + mapForStringDigits
+    val minimalDigit  = totalMap.get(totalMap.minOf { it -> it.key})!!.first().digitEnum
+    val maxDigit  = totalMap.get(totalMap.maxOf { it -> it.key})!!.first().digitEnum
+
+    return "${minimalDigit.digit}${maxDigit.digit}".toInt()
+
+}
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return input.map { it.filter { c -> c.isDigit()}}
+            .also(::println)
+            .map { "${it[0]}${it[it.length-1]}".toInt() }
+            .sum()
+    }
+
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { getCalibrationValue(it) }
+            .sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
+// ###############################################################
+    val input = readInput(
+//        "Day01-example"
+//        "Day01-input"
+//        "Day01-pt2-example"
+        "Day01-pt2-input"
+    )
+    println("#### Part 1 result : ")
+//    part1(input).println()
+    println("#### Part 2 result : ")
     part2(input).println()
 }
